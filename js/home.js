72f1a2ac075d6fa680b90ac003441c1e6ee7a151
NayanSarkar-A <web.nayansarkar2005@gmail.com>
2024-09-27 01:52:43 +0530

    Add Initial Files

---
+++ js/home.js
@@ -0,0 +1,22 @@
+let body=document.querySelector("body")
+let bar=document.querySelector("#bar");
+let mBar=document.querySelector(".nav-manu ");
+let close=document.querySelector("#close");
+let turn =true;
+
+bar.addEventListener("click",()=>{
+    if (turn){
+        mBar.style.right="0px"
+    }
+})
+close.addEventListener("click",()=>{
+    if (turn){
+        mBar.style.right="-3000px"
+    }
+})
+mBar.addEventListener("click",()=>{
+    if (turn){
+        mBar.style.right="-4000px"
+    }
+})
+
